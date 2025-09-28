2aa63fffd96ad42b2580c19cbcd98786255fdcea
Godwin Kamau <contactgodwinkamau@gmail.com>
2025-09-27 21:03:33 -0400

    added button to reset the game

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 //class for players objects for wins??
 
 const squares = document.querySelectorAll('.square')
+document.querySelector('button').addEventListener('click', gameReset)
 
 let player = 0
 let gameOver = false
@@ -23,9 +24,8 @@ const oWin = arrayElement => arrayElement === 'O'
 //Read through LivSterling's JS to find out about the relationship between querySelectorAll and forEach [https://github.com/LivSterling/morning-challenge-tic-tac-toe/blob/main/js/main.js]
 squares.forEach((square, i) => {
     square.addEventListener('click', () => {
-        if ( square.innerHTML === '' ) {
+        if ( square.innerHTML === '' && gameOver === false) {
             turnOrder(square , i)
-            console.log(i)
         }
     })
 })
@@ -51,11 +51,29 @@ function updateWin(i,player) {
         if (win.every(xWin)) {
             gameOver = true
             document.querySelector('h2').innerText = 'X wins!'
+            document.querySelector('button').style.display = 'block'
         } else if (win.every(oWin)) {
             gameOver = true
             document.querySelector('h2').innerText = 'O wins!'
+            document.querySelector('button').style.display = 'block'
         }
     })
-    console.log(winConditions)
+    
 }
 
+function gameReset() {
+    gameOver = false
+    player = 0
+    squares.forEach(square => {
+        square.innerHTML = ''
+    })
+    document.querySelector('h2').innerText = ''
+    winConditions.splice(0,winConditions.length,[ 0 , 1 , 2 ],
+    [ 3 , 4 , 5 ],
+    [ 6 , 7 , 8 ],
+    [ 0 , 4 , 8 ],
+    [ 6 , 4 , 2 ],
+    [ 0 , 3 , 6 ],
+    [ 1 , 4 , 7 ],
+    [ 2 , 5 , 8 ])
+}
